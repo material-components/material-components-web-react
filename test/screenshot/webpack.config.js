883f07dbc2b9a7ed342637edfacf838190b30512
lynnjepsen <lynnjepsen@google.com>
2018-03-09 10:02:11 -0800

    specify webpack bundles within test directories, avoid merge conflicts

--- test/screenshot/webpack.config.js
+++ test/screenshot/webpack.config.js
@@ -1,6 +1,5 @@
-const {bundle} = require('./webpack-bundles');
+const temporaryPackageBundles = require('./temporary-package/webpack.config.js');
 
 module.exports = [
-  bundle('temporary-package/index.js', 'temporary-package/bundle'),
-  bundle('temporary-package/foo.js', 'temporary-package/foo'),
+  ...temporaryPackageBundles,
 ];
