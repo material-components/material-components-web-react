1b876d2d94a3e00f4fdd5fb9888eb9c0176cb08a
Matt Goo <mattgoo@google.com>
2018-03-15 12:50:04 -0700

    test: added karma local and ci

---
+++ karma.ci.js
@@ -0,0 +1,9 @@
+const karmaConfig = require('./scripts/karma/config');
+
+// enable ChromeHeadless Browser
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+module.exports = function(config) {
+  karmaConfig.browsers = ['ChromeHeadless'];
+  config.set(karmaConfig);
+};
