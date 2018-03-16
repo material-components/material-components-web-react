2647f69cb63095db76d069a8a93d47eb2e0ecc99
Patrick RoDee <prodee@google.com>
2018-03-16 10:30:16 -0700

    WIP testing no anti-aliasing on all linux

--- test/screenshot/screenshot.js
+++ test/screenshot/screenshot.js
@@ -48,6 +48,7 @@ export default class Screenshot {
           errorType: 'movementDifferenceIntensity',
           transparency: 0.3,
         },
+        ignore: ['antialiasing'],
       };
 
       const data = await compareImages(newScreenshot, oldScreenshot, options);
