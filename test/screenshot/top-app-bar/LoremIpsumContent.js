ff6fa6a8c9569d8662a892f4d734775e6a80755d
Matt Goo <mattgoo@google.com>
2018-04-02 14:40:01 -0700

    WIP: remove word for sub pixel rendering

--- test/screenshot/top-app-bar/LoremIpsumContent.js
+++ test/screenshot/top-app-bar/LoremIpsumContent.js
@@ -4,11 +4,11 @@ export default () => {
   const createDemoParagraph = (key) => (<p className='demo-paragraph' key={key}>
     Pellentesque habitant morbi tristique senectus et netus et malesuada fames
     ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
-    tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
-    ultricies mi vitae est. Pellentesque habitant morbi tristique senectus et
+    tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
+    Ultricies mi vitae est. Pellentesque habitant morbi tristique senectus et
     netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat
     vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-    egestas semper. Aenean ultricies mi vitae est.
+    egestas semper. Ultricies mi vitae est.
   </p>);
   const size = 20;
   const tags = Array.from(Array(size).keys());
