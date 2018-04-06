78eabbeeeb4b0f5599872e7a2da57e889f3fa2d8
Matt Goo <mattgoo@google.com>
2018-04-06 10:57:18 -0700

    WIP: missing materialicons import

--- test/screenshot/top-app-bar/standard.js
+++ test/screenshot/top-app-bar/standard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TopAppBar from '../../../packages/top-app-bar';
+import MaterialIcon from '../../../packages/material-icon';
+
 import '../../../packages/top-app-bar/index.scss';
 import '../../../packages/material-icon/index.scss';
 import './index.scss';
