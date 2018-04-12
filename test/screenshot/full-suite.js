f5f8183238627d53ddc0e064d99c283658273857
Matt Goo <mattgoo@google.com>
2018-04-12 09:35:30 -0700

    WIP: removed more ripple screenshot deps

--- test/screenshot/full-suite.js
+++ test/screenshot/full-suite.js
@@ -1,12 +1,10 @@
 import buttonSuite from './button/screenshot-suite';
-import rippleSuite from './ripple/screenshot-suite';
 import materialIconSuite from './material-icon/screenshot-suite';
 import topAppBarSuite from './top-app-bar/screenshot-suite';
 
 const fullSuite = [
   buttonSuite,
   materialIconSuite,
-  rippleSuite,
   topAppBarSuite,
 ];
 
