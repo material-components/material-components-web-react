e48303673b9a425cb343b64fcd97d46673656161
Matt Goo <mattgoo@google.com>
2018-04-19 15:12:42 -0700

    WIP: tests

--- test/unit/material-icon/index.test.js
+++ test/unit/material-icon/index.test.js
@@ -7,8 +7,16 @@ suite('MaterialIcon');
 
 test('classNames adds classes', () => {
   const wrapper = mount(<MaterialIcon className='test-class-name'/>);
-  assert.isTrue(wrapper.hasClass('test-class-name'));
-  assert.equal(wrapper.find('.material-icons').length, 1);
+  const icon = wrapper.find('.material-icons');
+  assert.isTrue(icon.hasClass('test-class-name'));
+  assert.equal(icon.length, 1);
+});
+
+test('classNames adds ripple class if hasRipple is true', () => {
+  const wrapper = mount(<MaterialIcon hasRipple className='test-class-name'/>);
+  const icon = wrapper.find('.material-icons');
+  assert.isTrue(icon.hasClass('test-class-name'));
+  assert.isTrue(icon.hasClass('material-icons--ripple-surface'));
 });
 
 test('has icon type as child', () => {
@@ -16,16 +24,18 @@ test('has icon type as child', () => {
   assert.equal(wrapper.find('.material-icons').text(), 'menu');
 });
 
+const rippledIconComponent = <RippleMaterialIcon hasRipple unbounded icon='menu' />;
+
 test('if hasRipple true, then it should contain RippleMaterialIcon', () => {
   const wrapper = mount(
     <MaterialIcon icon='menu' hasRipple />
   );
-  assert.isTrue(wrapper.contains(<RippleMaterialIcon unbounded icon='menu' />));
+  assert.isTrue(wrapper.contains(rippledIconComponent));
 });
 
 test('if hasRipple false, then it should not contain RippleMaterialIcon', () => {
   const wrapper = mount(
     <MaterialIcon icon='menu' />
   );
-  assert.isFalse(wrapper.contains(<RippleMaterialIcon unbounded icon='menu' />));
+  assert.isFalse(wrapper.contains(rippledIconComponent));
 });
