8af4eb22327b84671318b051918cf26cae9629c9
Matt Goo <mattgoo@google.com>
2018-04-20 16:04:06 -0700

    WIP: initial line ripple commit

---
+++ packages/line-ripple/README.md
@@ -0,0 +1,15 @@
+# React Line Ripple
+
+MDC React Line Ripple is a component for material icons.
+
+## Installation
+
+```
+npm install @material/react-line-ripple
+```
+
+## Props
+
+Prop Name | Type | Description
+--- | --- | ---
+className | String | Classes to be applied to the root element.
---
+++ packages/line-ripple/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+export default class LineRipple extends Component {
+  render() {
+
+  }
+}
+
+LineRipple.propTypes = {
+  className: PropTypes.string,
+};
+
+LineRipple.defaultProps = {
+  className: '',
+};
