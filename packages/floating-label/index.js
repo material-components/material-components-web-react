09dfda6b494ed4ec25c783ef2b8fc3d8a3597fa2
Matt Goo <mattgoo@google.com>
2018-04-20 10:52:53 -0700

    WIP: screenshot test for floating label

--- packages/floating-label/index.js
+++ packages/floating-label/index.js
@@ -7,16 +7,44 @@ export default class FloatingLabel extends React.Component {
 
   foundation_ = null;
 
+  constructor(props) {
+    super(props);
+    this.labelElement = React.createRef();
+  }
+
   state = {
     classList: new Set(),
   };
 
   componentDidMount() {
     this.initializeFoundation();
+    this.setWidth();
+
+    if (this.props.shouldFloat) {
+      this.foundation_.float(true);
+    }
+    if (this.props.shouldShake) {
+      this.foundation_.shake(true);
+    }
   }
 
   componentWillUnmount() {
     this.foundation_.destroy();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.shouldFloat !== nextProps.shouldFloat) {
+      this.foundation_.float(nextProps.shouldFloat);
+    }
+    if (this.props.shouldShake !== nextProps.shouldShake) {
+      this.foundation_.shake(nextProps.shouldShake);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.children !== prevProps.children) {
+      this.setWidth();
+    }
   }
 
   initializeFoundation = () => {
@@ -34,22 +62,49 @@ export default class FloatingLabel extends React.Component {
     return {
       addClass: (className) =>
         this.setState({classList: this.state.classList.add(className)}),
-      removeClass: (className) => {
-        const {classList} = this.state;
-        classList.delete(className);
-        this.setState({classList});
-      },
-      getWidth: () => {
-
-      },
+      removeClass: this.removeClassFromClassList,
     };
   }
 
+  removeClassFromClassList = (className) => {
+    const {classList} = this.state;
+    classList.delete(className);
+    this.setState({classList});
+  }
+
+  setWidth = () => {
+    const {setWidth} = this.props;
+    if (setWidth && typeof setWidth === 'function') {
+      setWidth(this.labelElement.current.offsetWidth);
+    }
+  }
+
+  onShakeEnd = () => {
+    const {onShakeEnd} = this.props;
+    const {LABEL_SHAKE} = MDCFloatingLabelFoundation.cssClasses;
+    this.removeClassFromClassList(LABEL_SHAKE);
+    if (onShakeEnd && typeof onShakeEnd === 'function') {
+      onShakeEnd();
+    }
+  }
+
   render() {
+    const {
+      className,
+      children,
+      onShakeEnd,
+      setWidth,
+      shouldFloat,
+      shouldShake,
+      ...otherProps
+    } = this.props;
+
     return (
       <label
         className={this.classes}
-        {...this.props}
+        ref={this.labelElement}
+        onAnimationEnd={this.onShakeEnd}
+        {...otherProps}
       >
         {children}
       </label>
@@ -60,8 +115,16 @@ export default class FloatingLabel extends React.Component {
 FloatingLabel.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node,
+  onShakeEnd: PropTypes.func,
+  setWidth: PropTypes.func,
+  shouldFloat: PropTypes.bool,
+  shouldShake: PropTypes.bool,
 };
 
 FloatingLabel.defaultProps = {
   className: '',
+  onShakeEnd: () => {},
+  setWidth: () => {},
+  shouldFloat: false,
+  shouldShake: false,
 };
--- test/screenshot/floating-label/index.js
+++ test/screenshot/floating-label/index.js
@@ -5,8 +5,80 @@ import FloatingLabel from '../../../packages/floating-label';
 import '../../../packages/floating-label/index.scss';
 import './index.scss';
 
+class Input extends React.Component {
+  state = {
+    shouldUsePrimaryLabel: true,
+    shouldShake: false,
+    shouldFloat: false,
+    width: 0,
+  };
+
+  toggleShake = () => {
+    this.setState({shouldShake: !this.state.shouldShake});
+  }
+
+  render() {
+    const {shouldUsePrimaryLabel, shouldFloat, shouldShake} = this.state;
+    const labelText = ['My Label Text', 'Alternate Label Text'];
+
+    return (
+      <div>
+        <div className='input__container'>
+          <div className='input__dock'>
+            <FloatingLabel
+              shouldFloat={shouldFloat}
+              shouldShake={shouldShake}
+              onShakeEnd={this.toggleShake}
+              setWidth={width => this.setState({width})}
+            >
+              {shouldUsePrimaryLabel ? labelText[0] : labelText[1]}
+            </FloatingLabel>
+          </div>
+          {this.renderControls()}
+        </div>
+      </div>
+    )
+  }
+
+  renderControls() {
+    const {width} = this.state;
+    const buttons = [{
+      text: 'Float',
+      action: () => this.setState({shouldFloat: !this.state.shouldFloat}),
+    }, {
+      text: 'Shake',
+      action: this.toggleShake,
+    }, {
+      text: 'Label Text',
+      action: () => this.setState({shouldUsePrimaryLabel: !this.state.shouldUsePrimaryLabel}),
+    }]
+
+    return (
+      <div className='input__controls'>
+        {buttons.map((button, index) => (
+          <button
+            key={index}
+            onClick={button.action}
+            className='mdc-button mdc-button--raised input__control-button'>
+            {button.text}
+          </button>
+        ))}
+        <div>
+          Label Width: {width}
+        </div>
+      </div>
+    );
+  }
+};
+
 ReactDOM.render((
     <div>
+      <Input />
 
+      <div className='floated-label-box-example'>
+        <FloatingLabel shouldFloat>
+          Floated Label
+        </FloatingLabel>
+      </div>
     </div>
 ), document.getElementById('app'));
