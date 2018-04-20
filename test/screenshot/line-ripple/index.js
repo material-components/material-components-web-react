3cf51b000bdecd6135a56dc52ae6e11e45430a2d
Matt Goo <mattgoo@google.com>
2018-04-20 16:07:27 -0700

    WIP: screenshot test line ripple

---
+++ test/screenshot/line-ripple/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../../../packages/line-ripple/index.scss';
+import './index.scss';
+
+import Button from '../../../packages/button';
+
+ReactDOM.render((
+  <div>
+
+  </div>
+), document.getElementById('app'));
