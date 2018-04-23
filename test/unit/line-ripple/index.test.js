a26a6292d89964ed3f522c9c08a6cefacf7a81f3
Matt Goo <mattgoo@google.com>
2018-04-23 13:16:50 -0700

    WIP: tests unit

---
+++ test/unit/line-ripple/index.test.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import td from 'testdouble';
+import {assert} from 'chai'
+import {shallow} from 'enzyme';
+import LineRipple from '../../../packages/line-ripple';
+
+suite('LineRipple');
+
+test('classNames adds classes', () => {
+  const wrapper = shallow(
+    <LineRipple className='test-class-name' />);
+  assert.isTrue(wrapper.hasClass('test-class-name'));
+  assert.isTrue(wrapper.hasClass('mdc-line-ripple'));
+});
+
+test('retains both state and props styles', () => {
+  const wrapper = shallow(<LineRipple />);
+  wrapper.setProps({style: {color: 'orange'}});
+  wrapper.setState({style: {background: 'red'}});
+  assert.deepEqual(wrapper.props().style,
+    {background: 'red', color: 'orange'});
+});
+
+test('creates foundation', () => {
+  const wrapper = shallow(<LineRipple />);
+  assert.exists(wrapper.instance().foundation_);
+});
+
+test('#componentWillReceiveProps #foundation activate & deactivate ' +
+  'are called when shouldActivate updates', () => {
+  const wrapper = shallow(<LineRipple />);
+  wrapper.instance().foundation_.activate = td.func();
+  wrapper.instance().foundation_.deactivate = td.func();
+
+  wrapper.setProps({shouldActivate: true});
+  td.verify(wrapper.instance().foundation_.activate(), {times: 1});
+  td.verify(wrapper.instance().foundation_.deactivate(), {times: 0});
+
+  wrapper.setProps({shouldActivate: false});
+  td.verify(wrapper.instance().foundation_.activate(), {times: 1});
+  td.verify(wrapper.instance().foundation_.deactivate(), {times: 1});
+});
+
+test('#adapter.addClass updates state.classList', () => {
+  const wrapper = shallow(<LineRipple />);
+  wrapper.instance().foundation_.adapter_.addClass('test-color-class');
+  assert.isTrue(wrapper.state().classList.has('test-color-class'));
+});
+
+test('#adapter.removeClass updates state.classList', () => {
+  const wrapper = shallow(<LineRipple />);
+  const classList = new Set();
+  classList.add('test-color-class');
+  wrapper.setState({classList});
+  wrapper.instance().foundation_.adapter_.removeClass('test-color-class');
+  assert.isFalse(wrapper.state().classList.has('test-color-class'));
+});
+
+test('#adapter.hasClass returns true if exists in classList', () => {
+  const wrapper = shallow(<LineRipple />);
+  const classList = new Set();
+  classList.add('test-color-class');
+  wrapper.setState({classList});
+
+  const hasClass = wrapper.instance().foundation_.adapter_.hasClass('test-color-class');
+  assert.isTrue(hasClass);
+});
+
+test('#adapter.setStyle updates style', () => {
+  const wrapper = shallow(<LineRipple />);
+  wrapper.instance().foundation_.adapter_.setStyle('color', 'blue');
+  assert.equal(wrapper.state().style.color, 'blue');
+});
+
+test('onTransitionEnd calls the #foundation.handleTransitionEnd', () => {
+  const wrapper = shallow(<LineRipple />);
+  wrapper.instance().foundation_.handleTransitionEnd = td.func();
+  const event = {test: '123'};
+  wrapper.simulate('transitionEnd', event);
+  td.verify(
+    wrapper.instance().foundation_.handleTransitionEnd(event), {times: 1});
+});
+
+test('#componentWillUnmount destroys foundation', () => {
+  const wrapper = shallow(<LineRipple />);
+  const foundation = wrapper.instance().foundation_;
+  foundation.destroy = td.func();
+  wrapper.unmount();
+  td.verify(foundation.destroy());
+});
