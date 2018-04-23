c95d4ae539970283829dbc03a21402520914fd2c
Matt Goo <mattgoo@google.com>
2018-04-23 16:50:03 -0700

    WIP: updated screenshot tests - cleanup

--- test/screenshot/line-ripple/index.js
+++ test/screenshot/line-ripple/index.js
@@ -5,36 +5,16 @@ import './index.scss';
 
 import LineRipple from '../../../packages/line-ripple';
 
-class Line extends React.Component {
-  state = {
-    active: false,
-  };
-
-  render() {
-    return (
-      <div
-        onClick={() => this.setState({active: !this.state.active})}
-        className='line-ripple-container'>
-        <LineRipple active={this.state.active} />
-      </div>
-    );
-  }
-};
-
-class LineActive extends React.Component {
-  render() {
-    return (
-      <div
-        className='line-ripple-container'>
-        <LineRipple active />
-      </div>
-    );
-  }
-}
-
 ReactDOM.render((
   <div>
-    <Line />
-    <LineActive />
+    <div
+      className='line-ripple-container'>
+      <LineRipple />
+    </div>
+
+    <div
+      className='line-ripple-container'>
+      <LineRipple active />
+    </div>
   </div>
 ), document.getElementById('app'));
