c8c3720d5da49e67173ac777708bd4b6ba35ea3d
Matt Goo <mattgoo@google.com>
2018-04-30 09:46:54 -0700

    WIP: screenshot revert && fullsuite refactor

--- test/screenshot/full-suite.js
+++ test/screenshot/full-suite.js
@@ -1,23 +1,11 @@
-import buttonSuite from './button/screenshot-suite';
-import cardSuite from './card/screenshot-suite';
-import materialIconSuite from './material-icon/screenshot-suite';
-import topAppBarSuite from './top-app-bar/screenshot-suite';
-import fabSuite from './fab/screenshot-suite';
-import lineRippleSuite from './line-ripple/screenshot-suite';
-import floatingLabelSuite from './floating-label/screenshot-suite';
-import notchedOutlineSuite from './notched-outline/screenshot-suite';
-import textFieldIconSuite from './text-field/icon/screenshot-suite';
-
-const fullSuite = [
-  buttonSuite,
-  cardSuite,
-  materialIconSuite,
-  topAppBarSuite,
-  fabSuite,
-  lineRippleSuite,
-  floatingLabelSuite,
-  notchedOutlineSuite,
-  textFieldIconSuite,
+export default [
+  require('./button/screenshot-suite'),
+  require('./card/screenshot-suite'),
+  require('./material-icon/screenshot-suite'),
+  require('./top-app-bar/screenshot-suite'),
+  require('./fab/screenshot-suite'),
+  require('./line-ripple/screenshot-suite'),
+  require('./floating-label/screenshot-suite'),
+  require('./notched-outline/screenshot-suite'),
+  require('./text-field/icon/screenshot-suite'),
 ];
-
-export default fullSuite;
