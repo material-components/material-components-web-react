8580cc63e6931e4c54d6806f1db7c571ec4ffe72
Matt Goo <mattgoo@google.com>
2018-05-08 13:49:47 -0700

    WIP: textfield screenshots

--- test/screenshot/webpack.config.js
+++ test/screenshot/webpack.config.js
@@ -9,6 +9,5 @@ module.exports = [
   ...require('./text-field/webpack.config.js'),
   ...require('./text-field/helper-text/webpack.config.js'),
   ...require('./text-field/icon/webpack.config.js'),
-  ...require('./text-field/webpack.config.js'),
   ...require('./top-app-bar/webpack.config.js'),
 ];
