d21a93131bce876681596d8ee1f1b2addd4af668
Matt Goo <mattgoo@google.com>
2018-06-05 10:35:55 -0700

    WIP: fix test:unit-ci

--- karma.ci.js
+++ karma.ci.js
@@ -1,9 +1,7 @@
 const karmaConfig = require('./scripts/karma/config');
 
 // enable ChromeHeadless Browser
-// process.env.CHROME_BIN = require('puppeteer').executablePath();
-// console.log(process.env.CHROME_BIN)
-process.env.CHROME_BIN = 'google-chrome-unstable';
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function(config) {
   karmaConfig.browsers = ['ChromeHeadless'];
