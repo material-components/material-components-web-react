8ed862f9b0d9eef03390caefe3feb309d9b2a249
Bonnie Zhou <bonniez@google.com>
2018-06-25 16:28:32 -0700

    tests

--- test/unit/chips/Chip.test.js
+++ test/unit/chips/Chip.test.js
@@ -9,16 +9,9 @@ suite('Chip');
 test('classNames adds classes', () => {
   const wrapper = mount(<Chip className='test-class-name' />);
   assert.isTrue(wrapper.hasClass('test-class-name'));
-  assert.isTrue(wrapper.hasClass('mdc-chip'));
 });
 
 test('renders chip', () => {
   const wrapper = mount(<Chip>Hello world</Chip>);
   assert.isOk(wrapper.find('.mdc-chip'));
-});
-
-test('default initRipple function', () => {
-  Chip.defaultProps.initRipple = td.func();
-  mount(<Chip />);
-  td.verify(Chip.defaultProps.initRipple(td.matchers.isA(Object)), {times: 1});
 });
