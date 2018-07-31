00714cd376d21c3fb971025303ad89878a785a06
Matt Goo <mattgoo@google.com>
2018-07-30 17:38:04 -0700

    WIP: remove bidirection

--- packages/bidirection/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-export const isElementRtl = (element) => {
-  if (element) {
-    const dir = window.getComputedStyle(element).getPropertyValue('direction');
-    return dir === 'rtl';
-  }
-}
