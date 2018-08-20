643716447356235652779bb105d5c28f74087d66
Matt Goo <mattgoo@google.com>
2018-08-20 15:36:44 -0700

    WIP: fix url

--- test/screenshot/screenshot-test-urls.js
+++ test/screenshot/screenshot-test-urls.js
@@ -1,25 +1,25 @@
 import topAppBarVariants from './top-app-bar/variants';
 
 const urls = [
-  '/button',
-  '/card',
-  // '/chips',
-  '/line-ripple',
-  '/fab',
-  '/floating-label',
-  '/material-icon',
-  '/notched-outline',
-  '/select',
-  '/tab',
-  '/tab-indicator',
-  '/tab-scroller',
-  '/text-field',
-  '/text-field/helper-text',
-  '/text-field/icon',
+  'button',
+  'card',
+  // 'chips',
+  'line-ripple',
+  'fab',
+  'floating-label',
+  'material-icon',
+  'notched-outline',
+  'select',
+  'tab',
+  'tab-indicator',
+  'tab-scroller',
+  'text-field',
+  'text-field/helper-text',
+  'text-field/icon',
 ];
 
 topAppBarVariants.forEach((variant) => {
-  urls.push(`/top-app-bar/${variant}`);
+  urls.push(`top-app-bar/${variant}`);
 });
 
 export default urls;
