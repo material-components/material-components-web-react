e0ebac846871b2b684c41e837b64ad8358020e88
Bonnie Zhou <bonniez@google.com>
2018-09-14 11:28:20 -0700

    feat(tab): Add ripple component

---
+++ packages/tab/TabRipple.js
@@ -0,0 +1,69 @@
+// The MIT License
+//
+// Copyright (c) 2018 Google, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import withRipple from '@material/react-ripple';
+
+export class TabRipple extends React.Component {
+
+  init = (el) => {
+    this.props.initRipple(el);
+  }
+
+  get classes() {
+    return classnames('mdc-tab__ripple', this.props.className);
+  }
+
+  render() {
+    const {
+      /* eslint-disable */
+      className,
+      unbounded,
+      /* eslint-enable */
+      initRipple,
+      ...otherProps
+    } = this.props;
+
+    return (
+      <div
+        className={this.classes}
+        ref={initRipple}
+        {...otherProps}
+      >
+      </div>
+    );
+  }
+}
+
+TabRipple.propTypes = {
+  initRipple: PropTypes.func,
+  unbounded: PropTypes.bool,
+};
+
+TabRipple.defaultProps = {
+  initRipple: () => {},
+  unbounded: false,
+};
+
+export default withRipple(TabRipple);
