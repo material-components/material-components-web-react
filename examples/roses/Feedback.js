33bbf3ad27e40434f94ceef1f4857c0f75ba9f84
Matt Goo <mattgoo@google.com>
2018-10-01 18:08:38 -0700

    WIP: roses

--- examples/roses/Feedback.js
+++ examples/roses/Feedback.js
@@ -12,10 +12,41 @@ class Feedback extends React.Component {
   render() {
     return (
       <div>
-        <ChipSet choice>
-           <Chip id='summer' label='Summer'/>
-           <Chip id='winter' label='Winter'/>
-         </ChipSet>
+        {this.renderHeader()}
+        <div className='mdc-top-app-bar--fixed-adjust'>
+          {this.renderContent()}
+          <ChipSet filter>
+            <Chip id='fast' label='Fast Delivery'/>
+            <Chip id='great_flowers' label='Great Flowers'/>
+            <Chip id='nice_courier' label='Nice Courier'/>
+            <Chip id='easy_to_use' label='Easy to Order'/>
+          </ChipSet>
+         </div>
+      </div>
+    );
+  }
+
+  renderHeader() {
+    return (
+      <TopAppBar
+        title='Feedback'
+        navigationIcon={<MaterialIcon
+          icon='close'
+          onClick={() => console.log('close feedback surface')}
+        />}
+      />
+    );
+  }
+
+  renderContent() {
+    return (
+      <div>
+        <h1 className='mdc-typography--headline5 mdc-theme--on-primary'>
+          Thanks for spreading joy with Red Roses
+        </h1>
+        <p className='mdc-typography--body2'>
+          We would love to hear about your ordering experience.
+        </p>
       </div>
     );
   }
