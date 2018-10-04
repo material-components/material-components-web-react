382b68ebcf2def0054771dd17b7f2704e7367153
Matt Goo <mattgoo@google.com>
2018-10-04 12:52:59 -0700

    WIP: drawer

--- packages/drawer/index.js
+++ packages/drawer/index.js
@@ -33,7 +33,34 @@ import FocusTrap from 'focus-trap-react';
 
 class Drawer extends React.Component {
   previousFocus_ = null;
+  foundation_ = null;
+  drawerElement_ = React.createRef();
+
   state = {classList: new Set()};
+
+  componentDidMount() {
+    const {dismissible, modal} = this.props;
+    if (dismissible) {
+      this.foundation_ = new MDCDismissibleDrawerFoundation(this.adapter);
+      this.foundation_.init();
+    } else if (modal) {
+      this.foundation_ = new MDCModalDrawerFoundation(this.adapter);
+      this.foundation_.init();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const {dismissible, modal, open} = this.props;
+    if (!(dismissible || modal)) return;
+
+    if (open !== prevProps.open) {
+      open ? this.foundation_.open() : this.foundation_.close();
+    }
+  }
+
+  componentWillUnmount() {
+    this.foundation_.destroy();
+  }
 
   get classes() {
     const {classList} = this.state;
@@ -61,17 +88,25 @@ class Drawer extends React.Component {
       },
       hasClass: (className) => this.classes.split(' ').includes(className),
       elementHasClass: (element, className) => element.classList.contains(className),
+      computeBoundingRect: () => {
+        const drawerElement = this.drawerElement_ && this.drawerElement_.current;
+        if (!drawerElement) return;
+        return drawerElement.getBoundingClientRect();
+      },
       saveFocus: () => {
         this.previousFocus_ = document.activeElement;
       },
       restoreFocus: () => {
         const previousFocus = this.previousFocus_ && this.previousFocus_.focus;
-        if (this.root_.contains(document.activeElement) && previousFocus) {
+        const drawerElement = this.drawerElement_ && this.drawerElement_.current;
+        if (drawerElement && previousFocus && drawerElement.contains(document.activeElement)) {
           previousFocus();
         }
       },
       focusActiveNavigationItem: () => {
-        const activeNavItemEl = this.root_.querySelector(`.${MDCListFoundation.cssClasses.LIST_ITEM_ACTIVATED_CLASS}`);
+        const drawerElement = this.drawerElement_ && this.drawerElement_.current;
+        if (!drawerElement) return;
+        const activeNavItemEl = drawerElement.querySelector(`.${MDCListFoundation.cssClasses.LIST_ITEM_ACTIVATED_CLASS}`);
         if (activeNavItemEl) {
           activeNavItemEl.focus();
         }
@@ -82,12 +117,27 @@ class Drawer extends React.Component {
       releaseFocus: () => this.setState({activeTrap: false}),
     };
   }
+
+  handleKeyDown = (evt) => {
+    this.props.onKeyDown(evt);
+    if (!this.foundation_) return;
+    this.foundation_.handleKeydown(evt);
+  }
+
+  handleTransitionEnd = (evt) => {
+    this.props.onTransitionEnd(evt);
+    if (!this.foundation_) return;
+    this.foundation_.handleTransitionEnd(evt);
+  }
+
   render() {
     const {activeTrap} = this.state;
     const {
       /* eslint-disable no-unused-vars */
       onClose,
       onOpen,
+      onKeyDown,
+      onTransitionEnd,
       dismissible,
       children,
       /* eslint-enable no-unused-vars */
@@ -97,14 +147,27 @@ class Drawer extends React.Component {
     } = this.props;
 
     return (
-      <DrawerElement className={this.classes}>
+      <DrawerElement
+        className={this.classes} ref={this.drawerElement_}
+        onKeyDown={(evt) => this.handleKeyDown(evt)}
+        onTransitionEnd={(evt) => this.handleTransitionEnd(evt)}
+      >
         {activeTrap ? (
           <FocusTrap>
             {children}
           </FocusTrap>
         ): children}
-        {modal ? <div className='mdc-drawer-scrim'></div> : null}
+        {modal ? this.renderScrim() : null}
       </DrawerElement>
+    );
+  }
+
+  renderScrim() {
+    return (
+      <div
+        className='mdc-drawer-scrim'
+        onClick={() => this.foundation_.handleScrimClick()}
+      ></div>
     );
   }
 }
@@ -113,6 +176,8 @@ class Drawer extends React.Component {
 Drawer.propTypes = {
   onOpen: PropTypes.func,
   onClose: PropTypes.func,
+  onTransitionEnd: PropTypes.func,
+  onKeyDown: PropTypes.func,
   drawerElement: PropTypes.string,
   dismissible: PropTypes.bool,
   modal: PropTypes.bool,
@@ -121,6 +186,8 @@ Drawer.propTypes = {
 Drawer.defaultProps = {
   onOpen: () => {},
   onClose: () => {},
+  onTransitionEnd: () => {},
+  onKeyDown: () => {},
   drawerElement: 'aside',
   dismissible: false,
   modal: false,
--- test/screenshot/drawer/index.js
+++ test/screenshot/drawer/index.js
@@ -1,16 +1,114 @@
 import React from 'react';
 import '../../../packages/drawer/index.scss';
+import './index.scss';
 
-import Drawer from '../../../packages/drawer/index';
+import TopAppBar from '../../../packages/top-app-bar/index';
+import MaterialIcon from '../../../packages/material-icon/index';
+import Drawer, {DrawerAppContent, DrawerHeader, DrawerSubtitle, DrawerTitle, DrawerContent} from '../../../packages/drawer/index';
 
-const DrawerScreenshotTest = () => {
-  return (
-    <div>
-      <Drawer>
-        YO
-      </Drawer>
-    </div>
-  );
+class DrawerScreenshotTest extends React.Component {
+  state = {open: false};
+
+  render() {
+    return (
+      <div className='drawer-screenshot-test'>
+        <Drawer dismissible open={this.state.open}>
+          <DrawerHeader>
+            <DrawerTitle>
+              Inbox
+            </DrawerTitle>
+            <DrawerSubtitle>
+              ralph@gmail.com
+            </DrawerSubtitle>
+          </DrawerHeader>
+
+          <DrawerContent>
+            {this.renderList()}
+          </DrawerContent>
+        </Drawer>
+
+        <DrawerAppContent>
+          <TopAppBar
+            title='Email'
+            navigationIcon={<MaterialIcon onClick={() => this.setState({open: !this.state.open})} icon='menu' />}
+          />
+          <div className='mdc-top-app-bar--fixed-adjust'>
+            {this.renderLoremIpsum()}
+          </div>
+        </DrawerAppContent>
+      </div>
+    );
+  }
+
+  renderLoremIpsum() {
+    return (
+      <p className='drawer-lorem-ipsum'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br />
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut<br />
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut<br />
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in<br />
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint<br />
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit<br />
+        anim id est laborum.
+      </p>
+    );
+  }
+
+  renderList() {
+    const topItems = [{
+      title: 'Inbox', icon: 'inbox', activated: true,
+    }, {
+      title: 'Star', icon: 'star',
+    }, {
+      title: 'Sent Mail', icon: 'send',
+    }, {
+      title: 'Drafts', icon: 'drafts',
+    }];
+
+    const middleItems = [{
+      title: 'Family', icon: 'bookmark',
+    }, {
+      title: 'Friends', icon: 'bookmark',
+    }, {
+      title: 'Work', icon: 'bookmark',
+    }];
+
+    const bottomItems = [{
+      title: 'Settings', icon: 'settings',
+    }, {
+      title: 'Help & feedback', icon: 'announcement',
+    }];
+
+    return (
+      <nav className='mdc-list'>
+        {topItems.map(this.renderListItem)}
+
+        <hr className='mdc-list-divider' />
+        <h6 className='mdc-list-group__subheader'>Labels</h6>
+        {middleItems.map(this.renderListItem)}
+
+        <hr className='mdc-list-divider' />
+        {bottomItems.map(this.renderListItem)}
+      </nav>
+    );
+  }
+
+  renderListItem({title, icon, activated}, index) {
+    return (
+      <a
+        key={index}
+        className={`mdc-list-item {activated ? 'mdc-list-item--activated' : ''}`}
+        aria-selected='{activated}' tabIndex={activated ? '0' : ''}>
+        <i className='material-icons mdc-list-item__graphic' aria-hidden='true'>
+          {icon}
+        </i>
+        <span className='mdc-list-item__text'>
+          {title}
+          <i className='test-font--redact-prev-letter'></i>
+        </span>
+      </a>
+    );
+  }
 };
 
 export default DrawerScreenshotTest;
