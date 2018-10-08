cfea2f54397e029e0008064434b2e8b6b35befc6
Matt Goo <mattgoo@google.com>
2018-10-08 16:52:46 -0700

    fix: screenshots

--- test/screenshot/screenshot.js
+++ test/screenshot/screenshot.js
@@ -201,6 +201,7 @@ export default class Screenshot {
   async takeScreenshot_() {
     const browser = await puppeteer.launch({
       executablePath: 'google-chrome-unstable',
+      // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#tips
       args: ['--disable-dev-shm-usage'],
     });
     const page = await browser.newPage();
