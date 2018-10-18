105d9b3d87c2a19eb7a4d1058a4ad0c9003e2bc6
Hendrik Volschenk <righteous.trespasser@gmail.com>
2018-10-18 09:10:36 +0200

    feat(layout-grid): add ordering to cells

--- packages/layout-grid/Cell.js
+++ packages/layout-grid/Cell.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Cell = (props) => {
-  const {align, children, className, columns, desktop, phone, tablet, ...otherProps} = props;
+  const {align, children, className, columns, desktop, order, phone, tablet, ...otherProps} = props;
 
   const classes = classnames('mdc-layout-grid__cell', className, {
     [`mdc-layout-grid__cell--align-${align}`]: !!align,
+    [`mdc-layout-grid__cell--order-${order}`]: !!order,
     [`mdc-layout-grid__cell--span-${columns}`]: !!columns,
     [`mdc-layout-grid__cell--span-${desktop}-desktop`]: !!desktop,
     [`mdc-layout-grid__cell--span-${phone}-phone`]: !!phone,
@@ -26,6 +27,7 @@ Cell.propTypes = {
   className: PropTypes.string,
   columns: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
   desktop: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+  order: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
   phone: PropTypes.oneOf([1, 2, 3, 4]),
   tablet: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8]),
 };
@@ -36,6 +38,7 @@ Cell.defaultProps = {
   className: '',
   columns: undefined,
   desktop: undefined,
+  order: undefined,
   phone: undefined,
   tablet: undefined,
 };
