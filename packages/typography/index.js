e179f31150117007bdfd27be7c6e94acd8f38404
Hendrik Volschenk <righteous.trespasser@gmail.com>
2018-11-01 22:24:34 +0200

    fix(typography): swap single components to higher-order-component
    create single higher-order-component instead of loose components
    update faceace to export from higher-order-component

--- packages/typography/index.js
+++ packages/typography/index.js
@@ -20,16 +20,18 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-export {default as Body1} from './Body1';
-export {default as Body2} from './Body2';
-export {default as Button} from './Button';
-export {default as Caption} from './Caption';
-export {default as Headline1} from './Headline1';
-export {default as Headline2} from './Headline2';
-export {default as Headline3} from './Headline3';
-export {default as Headline4} from './Headline4';
-export {default as Headline5} from './Headline5';
-export {default as Headline6} from './Headline6';
-export {default as Overline} from './Overline';
-export {default as Subtitle1} from './Subtitle1';
-export {default as Subtitle2} from './Subtitle2';
+import typographyHOC from './typography';
+
+export const Body1 = typographyHOC({classModifier: 'body1', tag: 'p'});
+export const Body2 = typographyHOC({classModifier: 'body2', tag: 'p'});
+export const Button = typographyHOC({classModifier: 'button', tag: 'span'});
+export const Caption = typographyHOC({classModifier: 'caption', tag: 'span'});
+export const Headline1 = typographyHOC({classModifier: 'headline1', tag: 'h1'});
+export const Headline2 = typographyHOC({classModifier: 'headline2', tag: 'h2'});
+export const Headline3 = typographyHOC({classModifier: 'headline3', tag: 'h3'});
+export const Headline4 = typographyHOC({classModifier: 'headline4', tag: 'h4'});
+export const Headline5 = typographyHOC({classModifier: 'headline5', tag: 'h5'});
+export const Headline6 = typographyHOC({classModifier: 'headline6', tag: 'h6'});
+export const Overline = typographyHOC({classModifier: 'overline', tag: 'span'});
+export const Subtitle1 = typographyHOC({classModifier: 'subtitle1', tag: 'h6'});
+export const Subtitle2 = typographyHOC({classModifier: 'subtitle2', tag: 'h6'});
