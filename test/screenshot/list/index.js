0f01fa74c71bc9be82596b2032e48932ef6bc4e8
Bonnie Zhou <bonniez@google.com>
2018-11-04 21:02:39 -0800

    add inserting list items to screenshot test

--- test/screenshot/list/index.js
+++ test/screenshot/list/index.js
@@ -8,29 +8,11 @@ import {ListItem} from '../../../packages/list/index';
 import ListItemGraphic from '../../../packages/list/ListItemGraphic';
 import ListItemText from '../../../packages/list/ListItemText';
 import ListItemMeta from '../../../packages/list/ListItemMeta';
-
-class SelectionListTest extends React.Component {
-  state = {
-    selectedIndex: 1,
-  };
-
-  render() {
-    const {children, ...otherProps} = this.props; // eslint-disable-line react/prop-types
-    return (
-      <List
-        singleSelection
-        selectedIndex={this.state.selectedIndex}
-        {...otherProps}
-      >
-        {children}
-      </List>
-    );
-  }
-}
+import uuidv1 from 'uuid/v1';
 
 const renderListItem = (primaryText, secondaryText) => {
   return (
-    <ListItem>
+    <ListItem key={uuidv1()}>
       <ListItemGraphic graphic={<MaterialIcon icon='folder'/>} />
       <ListItemText primaryText={primaryText} secondaryText={secondaryText}/>
       <ListItemMeta tabbableOnListItemFocus meta={<MaterialIcon icon='info'/>} />
@@ -38,22 +20,47 @@ const renderListItem = (primaryText, secondaryText) => {
   );
 };
 
+class SelectionListTest extends React.Component {
+  state = {
+    selectedIndex: 1,
+    listItems: ['List item 1', 'List item 2', 'List item 3'],
+  };
+
+  insertListItem = () => {
+    const {listItems, selectedIndex} = this.state;
+    listItems.splice(0, 0, 'New list item');
+    this.setState({listItems, selectedIndex: selectedIndex + 1});
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={this.insertListItem}>Insert new list item</button>
+        <List
+          {...this.props}
+          singleSelection
+          selectedIndex={this.state.selectedIndex}
+        >
+          {this.state.listItems.map((text) => renderListItem(text))}
+        </List>
+      </React.Fragment>
+    );
+  }
+}
+
 const ListScreenshotTest = () => {
   return (
     <div>
-      <h2>Two-line Selection List</h2>
-      <SelectionListTest twoLine>
+      <h2>One-line Selection List</h2>
+      <SelectionListTest />
+
+      <h2>Two-line List</h2>
+      <List twoLine>
         {renderListItem('hello', 'world')}
         {renderListItem('hello', 'world')}
         <li className='mdc-list-divider' role='separator'></li>
         {renderListItem('hello', 'world')}
-      </SelectionListTest>
-
-      <h2>One-line List</h2>
-      <List>
-        {renderListItem('hello')}
-        {renderListItem('hello')}
-        {renderListItem('hello')}
+        {renderListItem('hello', 'world')}
       </List>
     </div>
   );
