90adbbecbf8acf80b67e8523a885dd3cc832d2ab
Matt Goo <mattgoo@google.com>
2018-11-06 15:26:42 -0800

    fix(drawer): update readme and screenshots to support below top app bar (#397)

---
+++ test/screenshot/drawer/DrawerBelowTopAppBar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import '../../../packages/drawer/index.scss';
+import './index.scss';
+
+import TopAppBar, {TopAppBarFixedAdjust} from '../../../packages/top-app-bar/index';
+import Drawer, {
+  DrawerAppContent,
+  DrawerHeader,
+  DrawerSubtitle,
+  DrawerTitle,
+  DrawerContent,
+} from '../../../packages/drawer/index';
+import List from './List';
+
+const DrawerScreenshotTest = ({
+  /* eslint-disable react/prop-types */
+  onClose,
+  open,
+  title,
+  dismissible,
+  modal,
+  renderLoremIpsum,
+  renderNavigationIcon,
+  /* eslint-enable react/prop-types */
+}) => {
+  return (
+    <React.Fragment>
+      <TopAppBar
+        title={title}
+        navigationIcon={renderNavigationIcon()}
+      />
+
+      <TopAppBarFixedAdjust className='drawer-screenshot-test drawer-screenshot-test--is-below'>
+        <Drawer
+          open={open}
+          onClose={onClose}
+          dismissible={dismissible}
+        >
+          <DrawerHeader>
+            <DrawerTitle>
+              Inbox
+            </DrawerTitle>
+            <DrawerSubtitle>
+              ralph@gmail.com
+            </DrawerSubtitle>
+          </DrawerHeader>
+
+          <DrawerContent>
+            <List />
+          </DrawerContent>
+        </Drawer>
+
+        <DrawerAppContent className='drawer-app-content'>
+          {[0, 1, 2, 3, 4, 5].map(renderLoremIpsum)}
+        </DrawerAppContent>
+      </TopAppBarFixedAdjust>
+    </React.Fragment>
+  );
+};
+
+export default DrawerScreenshotTest;
