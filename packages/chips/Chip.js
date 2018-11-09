ed33550e17244906f0d4c0e577461126546475a2
Matt Goo <mattgoo@google.com>
2018-11-08 17:36:35 -0800

    fix: chips

--- packages/chips/Chip.js
+++ packages/chips/Chip.js
@@ -94,11 +94,11 @@ export class Chip extends Component {
       setStyleProperty: (propertyName, value) => this.chipElement_.style.setProperty(propertyName, value),
       notifyRemoval: () => this.props.handleRemove(this.props.id),
       notifyInteraction: () => this.props.handleInteraction(this.props.id),
-      notifySelection: () => {
+      notifySelection: (selected) => {
         if (this.state.interactionSelection) {
           this.setState({interactionSelection: false});
         } else {
-          this.props.handleSelect(this.props.id);
+          this.props.handleSelect(this.props.id, selected);
         }
       },
       addClassToLeadingIcon: (className) => {
