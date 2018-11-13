b968bbfe562a56f61d9e9f71a5bfe1f3a08ab0b4
Matt Goo <mattgoo@google.com>
2018-11-12 17:28:00 -0800

    fix(drawer): use react-list for screenshot test

--- test/screenshot/drawer/List.js
+++ test/screenshot/drawer/List.js
@@ -1,59 +1,96 @@
 import React from 'react';
+import MaterialIcon from '../../../packages/material-icon';
+import List, {
+  ListItem,
+  ListItemGraphic,
+  ListItemText,
+  ListItemMeta,
+  ListGroup,
+  ListGroupSubheader,
+} from '../../../packages/list/index';
+import uuidv1 from 'uuid/v1';
 
-const renderListItem = ({
-  title, icon, activated, // eslint-disable-line react/prop-types
-}, index) => {
+import '../../../packages/list/index.scss';
+
+
+const renderListItem = ({title, icon, activated}) => {
   return (
-    <a
-      key={index}
-      className={`mdc-list-item {activated ? 'mdc-list-item--activated' : ''}`}
-      aria-selected='{activated}' tabIndex={activated ? '0' : ''}>
-      <i className='material-icons mdc-list-item__graphic' aria-hidden='true'>
-        {icon}
-      </i>
-      <span className='mdc-list-item__text'>
-        {title}
-        <i className='test-font--redact-prev-letter'></i>
-      </span>
-    </a>
+    <ListItem key={uuidv1()}>
+      <ListItemGraphic graphic={<MaterialIcon icon={icon}/>} />
+      <ListItemText primaryText={title} />
+    </ListItem>
   );
 };
 
-export default () => {
-  const topItems = [{
-    title: 'Inbox', icon: 'inbox', activated: true,
-  }, {
-    title: 'Star', icon: 'star',
-  }, {
-    title: 'Sent Mail', icon: 'send',
-  }, {
-    title: 'Drafts', icon: 'drafts',
-  }];
 
-  const middleItems = [{
-    title: 'Family', icon: 'bookmark',
-  }, {
-    title: 'Friends', icon: 'bookmark',
-  }, {
-    title: 'Work', icon: 'bookmark',
-  }];
+// const renderListItem = ({
+//   title, icon, activated, // eslint-disable-line react/prop-types
+// }, index) => {
+//   return (
+//     <a
+//       key={index}
+//       className={`mdc-list-item {activated ? 'mdc-list-item--activated' : ''}`}
+//       aria-selected='{activated}' tabIndex={activated ? '0' : ''}>
+//       <i className='material-icons mdc-list-item__graphic' aria-hidden='true'>
+//         {icon}
+//       </i>
+//       <span className='mdc-list-item__text'>
+//         {title}
+//         <i className='test-font--redact-prev-letter'></i>
+//       </span>
+//     </a>
+//   );
+// };
 
-  const bottomItems = [{
-    title: 'Settings', icon: 'settings',
-  }, {
-    title: 'Help & feedback', icon: 'announcement',
-  }];
+class DrawerList extends React.Component {
+  state = {
+    selectedIndex: 1,
+  };
 
-  return (
-    <nav className='mdc-list'>
-      {topItems.map(renderListItem)}
+  render() {
+    const topItems = [{
+      title: 'Inbox', icon: 'inbox', activated: true,
+    }, {
+      title: 'Star', icon: 'star',
+    }, {
+      title: 'Sent Mail', icon: 'send',
+    }, {
+      title: 'Drafts', icon: 'drafts',
+    }];
 
-      <hr className='mdc-list-divider' />
-      <h6 className='mdc-list-group__subheader'>Labels</h6>
-      {middleItems.map(renderListItem)}
+    const middleItems = [{
+      title: 'Family', icon: 'bookmark',
+    }, {
+      title: 'Friends', icon: 'bookmark',
+    }, {
+      title: 'Work', icon: 'bookmark',
+    }];
 
-      <hr className='mdc-list-divider' />
-      {bottomItems.map(renderListItem)}
-    </nav>
-  );
+    const bottomItems = [{
+      title: 'Settings', icon: 'settings',
+    }, {
+      title: 'Help & feedback', icon: 'announcement',
+    }];
+
+    return (
+      <List
+        {...this.props}
+        singleSelection
+        selectedIndex={this.state.selectedIndex}
+        handleSelect={(selectedIndex) => this.setState({selectedIndex})}
+      >
+        {topItems.map(renderListItem)}
+
+        <hr className='mdc-list-divider' />
+
+        <ListGroupSubheader>Labels</ListGroupSubheader>
+        {middleItems.map(renderListItem)}
+
+        <hr className='mdc-list-divider' />
+        {bottomItems.map(renderListItem)}
+      </List>
+    );
+  }
 };
+
+export default DrawerList;
