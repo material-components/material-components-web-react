b3e65fdfc96f12d30d593d444cfab118609d5870
Hendrik Volschenk <righteous.trespasser@gmail.com>
2018-11-14 10:42:17 +0200

    feat(linear-progress): update screenshot tests to use all variants
    disable animations on screenshot test page
    add more samples to screenshot test page

--- test/screenshot/linear-progress/index.js
+++ test/screenshot/linear-progress/index.js
@@ -18,7 +18,6 @@ class Closable extends React.Component {
       <React.Fragment>
         <LinearProgress
           buffer={0.9}
-          bufferingDots={false}
           closed={this.state.closed}
           progress={0.8}
         />
@@ -29,47 +28,76 @@ class Closable extends React.Component {
 }
 
 const FullBuffer = () => {
-  return (
-    <LinearProgress
-      buffer={1}
-      bufferingDots={false}
-      progress={0.5}
-    />
-  );
+  return (<LinearProgress buffer={1} progress={0.5} />);
 };
 
-const Reversed = () => {
-  return (
-    <LinearProgress
-      buffer={0.8}
-      bufferingDots={false}
-      progress={0.7}
-      reversed
-    />
-  );
+const FullBufferReversed = () => {
+  return (<LinearProgress buffer={1} progress={0.5} reversed />);
+};
+
+const Indeterminate = () => {
+  return (<LinearProgress indeterminate />);
+};
+
+const IndeterminateReversed = () => {
+  return (<LinearProgress indeterminate reversed />);
 };
 
 const Static = () => {
-  return (
-    <LinearProgress
-      buffer={0.8}
-      bufferingDots={false}
-      progress={0.7}
-    />
-  );
+  return (<LinearProgress buffer={0.8} progress={0.7} />);
+};
+
+const StaticNoDots = () => {
+  return (<LinearProgress buffer={0.8} bufferingDots={false} progress={0.7} />);
+};
+
+const StaticReversed = () => {
+  return (<LinearProgress buffer={0.8} progress={0.7} reversed />);
+};
+
+const StaticReversedNoDots = () => {
+  return (<LinearProgress buffer={0.8} bufferingDots={false} progress={0.7} reversed />);
 };
 
 const LinearProgressScreenshot = () => {
   return (
-    <div className="linear-progress-sample__container">
-      Full buffer
-      <FullBuffer />
-      Static
-      <Static />
-      Reversed
-      <Reversed />
-      Closable
-      <Closable />
+    <div className="linear-progress-sample__container test-animation--paused">
+      <div className="linear-progress-sample__item">
+        <p className="linear-progress-sample__item__title">Full buffer</p>
+        <FullBuffer />
+      </div>
+      <div className="linear-progress-sample__item">
+        <p className="linear-progress-sample__item__title">Full buffer (reversed)</p>
+        <FullBufferReversed />
+      </div>
+      <div className="linear-progress-sample__item">
+        <p className="linear-progress-sample__item__title">Static</p>
+        <Static />
+      </div>
+      <div className="linear-progress-sample__item">
+        <p className="linear-progress-sample__item__title">Static (reversed)</p>
+        <StaticReversed />
+      </div>
+      <div className="linear-progress-sample__item">
+        <p className="linear-progress-sample__item__title">Static (no dots)</p>
+        <StaticNoDots />
+      </div>
+      <div className="linear-progress-sample__item">
+        <p className="linear-progress-sample__item__title">Static (reversed, no dots)</p>
+        <StaticReversedNoDots />
+      </div>
+      <div className="linear-progress-sample__item">
+        <p className="linear-progress-sample__item__title">Indeterminate</p>
+        <Indeterminate />
+      </div>
+      <div className="linear-progress-sample__item">
+        <p className="linear-progress-sample__item__title">Indeterminate (reversed)</p>
+        <IndeterminateReversed />
+      </div>
+      <div className="linear-progress-sample__item">
+        <p className="linear-progress-sample__item__title">Closable</p>
+        <Closable />
+      </div>
     </div>
   );
 };
