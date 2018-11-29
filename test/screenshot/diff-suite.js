cff9dbc6548d083fec3bb2d0928e37e6586797ef
Matt Goo <mattgoo@google.com>
2018-11-28 17:19:56 -0800

    WIP: max-listeners

--- test/screenshot/diff-suite.js
+++ test/screenshot/diff-suite.js
@@ -2,6 +2,7 @@ import Screenshot from './screenshot';
 import testUrls from './screenshot-test-urls';
 
 testUrls.forEach((url) => {
+  process.on('warning', e => console.warn(e.stack));
   const screenshot = new Screenshot(url);
   screenshot.diff();
 });
