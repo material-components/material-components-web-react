8f82c08366b16c883ce849e0dd92c29b2914a359
Matt Goo <mattgoo@google.com>
2018-11-29 11:14:24 -0800

    fix: suite

--- test/screenshot/capture-suite.js
+++ test/screenshot/capture-suite.js
@@ -4,9 +4,11 @@ import testUrls from './screenshot-test-urls';
 
 const browser = new Browser();
 browser.launch().then(async (launchedBrowser) => {
-  testUrls.forEach((url) => {
-    const screenshot = new Screenshot(url, launchedBrowser);
-    screenshot.capture();
-  });
+  suite('capture suite', () => {
+    testUrls.forEach((url) => {
+      const screenshot = new Screenshot(url, launchedBrowser);
+      screenshot.capture();
+    });
+  })
   await launchedBrowser.close();
 });
