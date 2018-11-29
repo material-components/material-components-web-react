9b8c7e7b7de04ddd16b3cf2cd65e706a19c32bf0
Matt Goo <mattgoo@google.com>
2018-11-29 12:01:12 -0800

    WIP: screenshot conversion

--- test/screenshot/diff-suite.js
+++
@@ -1,7 +0,0 @@
-import Screenshot from './screenshot';
-import testUrls from './screenshot-test-urls';
-
-testUrls.forEach((url) => {
-  const screenshot = new Screenshot(url);
-  screenshot.diff();
-});
--- test/screenshot/screenshot.tsx
+++ test/screenshot/screenshot.tsx
@@ -1,34 +1,29 @@
-import {Readable} from 'stream';
-import {createHash} from 'crypto';
-import {readFile, writeFile} from 'fs';
-import {promisify} from 'util';
-import puppeteer from 'puppeteer';
-import compareImages from 'resemblejs/compareImages';
-import {assert} from 'chai';
-import Storage from '@google-cloud/storage';
-
-import comparisonOptions from './screenshot-comparison-options';
-
+import { Readable } from "stream";
+import { createHash } from "crypto";
+import { readFile, writeFile } from "fs";
+import { promisify } from "util";
+import puppeteer from "puppeteer";
+import compareImages from "resemblejs/compareImages";
+import { assert } from "chai";
+import Storage from "@google-cloud/storage";
+import comparisonOptions from "./screenshot-comparison-options";
 const readFilePromise = promisify(readFile);
 const writeFilePromise = promisify(writeFile);
-
 const serviceAccountKey = process.env.MDC_GCLOUD_SERVICE_ACCOUNT_KEY;
 const branchName = process.env.MDC_BRANCH_NAME;
 const commitHash = process.env.MDC_COMMIT_HASH;
-const goldenFilePath = './test/screenshot/golden.json';
-const bucketName = 'screenshot-uploads';
+const goldenFilePath = "./test/screenshot/golden.json";
+const bucketName = "screenshot-uploads";
 const defaultMetadata = {
   commit: commitHash,
-  branch: branchName,
+  branch: branchName
 };
-
 const storage = new Storage({
-  credentials: JSON.parse(serviceAccountKey),
+  credentials: JSON.parse(serviceAccountKey)
 });
-
 const bucket = storage.bucket(bucketName);
-
 export default class Screenshot {
+  urlPath_ = null;
   /**
    * @param {string} urlPath The URL path to test
    */
@@ -37,7 +32,6 @@ export default class Screenshot {
     this.urlPath_ = urlPath;
     // TODO allow clients to specify capture-chrome options, like viewport size
   }
-
   /**
    * Captures a screenshot of the test URL and marks it as the new golden image
    */
@@ -45,15 +39,14 @@ export default class Screenshot {
     test(this.urlPath_, async () => {
       const golden = await this.takeScreenshot_();
       const goldenHash = this.generateImageHash_(golden);
-      const goldenPath = this.getImagePath_(goldenHash, 'golden');
+      const goldenPath = this.getImagePath_(goldenHash, "golden");
       await Promise.all([
         this.saveImage_(goldenPath, golden),
-        this.saveGoldenHash_(goldenHash),
+        this.saveGoldenHash_(goldenHash)
       ]);
       return;
     });
   }
-
   /**
    * Diffs a screenshot of the test URL with the existing golden screenshot
    */
@@ -61,34 +54,28 @@ export default class Screenshot {
     test(this.urlPath_, async () => {
       // Get the golden file path from the golden hash
       const goldenHash = await this.getGoldenHash_();
-      const goldenPath = this.getImagePath_(goldenHash, 'golden');
-
+      const goldenPath = this.getImagePath_(goldenHash, "golden");
       // Take a snapshot and download the golden iamge
       const [snapshot, golden] = await Promise.all([
         this.takeScreenshot_(),
-        this.readImage_(goldenPath),
+        this.readImage_(goldenPath)
       ]);
-
       // Compare the images
       const data = await compareImages(snapshot, golden, comparisonOptions);
       const diff = data.getBuffer();
-
       // Use the same hash for the snapshot path and diff path so it's easy can associate the two
       const snapshotHash = this.generateImageHash_(snapshot);
-      const snapshotPath = this.getImagePath_(snapshotHash, 'snapshot');
-      const diffPath = this.getImagePath_(snapshotHash, 'diff');
-      const metadata = {golden: goldenHash};
-
+      const snapshotPath = this.getImagePath_(snapshotHash, "snapshot");
+      const diffPath = this.getImagePath_(snapshotHash, "diff");
+      const metadata = { golden: goldenHash };
       // Save the snapshot and the diff
       await Promise.all([
         this.saveImage_(snapshotPath, snapshot, metadata),
-        this.saveImage_(diffPath, diff, metadata),
+        this.saveImage_(diffPath, diff, metadata)
       ]);
-
       return assert.equal(Number(data.misMatchPercentage), 0);
     });
   }
-
   /**
    * Generates a unique hash from an image's contents
    * @param {!Buffer} imageBuffer The image buffer to hash
@@ -96,9 +83,10 @@ export default class Screenshot {
    * @private
    */
   generateImageHash_(imageBuffer) {
-    return createHash('sha256').update(imageBuffer).digest('hex');
+    return createHash("sha256")
+      .update(imageBuffer)
+      .digest("hex");
   }
-
   /**
    * Returns the golden hash
    * @return {string|undefined}
@@ -109,7 +97,6 @@ export default class Screenshot {
     const goldenJSON = JSON.parse(goldenFile);
     return goldenJSON[this.urlPath_];
   }
-
   /**
    * Returns the correct image path
    * @param {string} imageHash The image hash
@@ -118,15 +105,13 @@ export default class Screenshot {
    * @private
    */
   getImagePath_(imageHash, imageType) {
-    if (imageType === 'golden') {
+    if (imageType === "golden") {
       return `${this.urlPath_}/${imageHash}.golden.png`;
     }
-
-    if (['snapshot', 'diff'].includes(imageType)) {
+    if (["snapshot", "diff"].includes(imageType)) {
       return `${this.urlPath_}/${commitHash}/${imageHash}.${imageType}.png`;
     }
   }
-
   /**
    * Downloads an image from Google Cloud Storage
    * @param {string} gcsFilePath The file path on Google Cloud Storage
@@ -137,7 +122,6 @@ export default class Screenshot {
     const data = await bucket.file(gcsFilePath).download();
     return data[0];
   }
-
   /**
    * Saves the golden hash
    * @param {string} goldenHash The hash of the golden image
@@ -147,10 +131,9 @@ export default class Screenshot {
     const goldenFile = await readFilePromise(goldenFilePath);
     const goldenJSON = JSON.parse(goldenFile);
     goldenJSON[this.urlPath_] = goldenHash;
-    const goldenContent = JSON.stringify(goldenJSON, null, '  ');
+    const goldenContent = JSON.stringify(goldenJSON, null, "  ");
     await writeFilePromise(goldenFilePath, `${goldenContent}\r\n`);
   }
-
   /**
    * Saves the given image to Google Cloud Storage with optional metadata
    * @param {string} imagePath The path to the image
@@ -158,33 +141,32 @@ export default class Screenshot {
    * @param {!Object=} customMetadata Optional custom metadata
    * @private
    */
-  async saveImage_(imagePath, imageBuffer, customMetadata={}) {
+  async saveImage_(imagePath, imageBuffer, customMetadata = {}) {
     const metadata = Object.assign({}, defaultMetadata, customMetadata);
     const file = bucket.file(imagePath);
-
     // Check if file exists and exit if it does
     const [exists] = await file.exists();
     if (exists) {
-      console.log('✔︎ No changes to', imagePath);
+      console.log("✔︎ No changes to", imagePath);
       return;
     }
-
     // Create a new stream from the image buffer
     let stream = new Readable();
     stream.push(imageBuffer);
     stream.push(null);
-
     // The promise is resolved or rejected inside the stream event callbacks
     return new Promise((resolve, reject) => {
-      stream.pipe(file.createWriteStream())
-        .on('error', (err) => {
+      stream
+        .pipe(file.createWriteStream())
+        .on("error", err => {
           reject(err);
-        }).on('finish', async () => {
+        })
+        .on("finish", async () => {
           try {
             // Make the image public and set it's metadata
             await file.makePublic();
-            await file.setMetadata({metadata});
-            console.log('✔︎ Uploaded', imagePath);
+            await file.setMetadata({ metadata });
+            console.log("✔︎ Uploaded", imagePath);
             resolve();
           } catch (err) {
             reject(err);
@@ -192,7 +174,6 @@ export default class Screenshot {
         });
     });
   }
-
   /**
    * Takes a screenshot of the URL
    * @return {!Buffer}
@@ -200,14 +181,16 @@ export default class Screenshot {
    */
   async takeScreenshot_() {
     const browser = await puppeteer.launch({
-      executablePath: 'google-chrome-unstable',
+      executablePath: "google-chrome-unstable",
       // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#tips
-      args: ['--disable-dev-shm-usage'],
+      args: ["--disable-dev-shm-usage"]
     });
     const page = await browser.newPage();
-    await page.goto(`http://localhost:8080/#/${this.urlPath_}`, {'waitUntil': ['networkidle2']});
+    await page.goto(`http://localhost:8080/#/${this.urlPath_}`, {
+      waitUntil: ["networkidle2"]
+    });
     // await page.waitForSelector('#screenshot-test-app');
-    const imageBuffer = await page.screenshot({fullPage: true});
+    const imageBuffer = await page.screenshot({ fullPage: true });
     await browser.close();
     return imageBuffer;
   }
