c893f8e05719af6001981d34448c9d2d03682aae
Matt Goo <mattgoo@google.com>
2018-11-29 10:47:14 -0800

    WIP: test

--- test/screenshot/browser.js
+++ test/screenshot/browser.js
@@ -3,8 +3,8 @@ import puppeteer from 'puppeteer';
 export default class Browser {
 
   launch() {
-    return new Promise((resolve, reject) => {
-      const browser = puppeteer.launch({
+    return new Promise(async (resolve, reject) => {
+      const browser = await puppeteer.launch({
         executablePath: 'google-chrome-unstable',
         // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#tips
         args: ['--disable-dev-shm-usage'],
