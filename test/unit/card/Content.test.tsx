5768f35530f68edfc7a9a195d2d36d51c4d6ae47
Matt Goo <mattgoo@google.com>
2018-12-17 16:06:06 -0800

    fix: test

--- test/unit/card/Content.test.tsx
+++ test/unit/card/Content.test.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import * as React from 'react';
 import {assert} from 'chai';
 import {mount} from 'enzyme';
 import {CardPrimaryContent} from '../../../packages/card/index';
+// TODO: fix in #528
+// @ts-ignore
 import {createMockRaf} from '../helpers/raf';
 
 suite('CardPrimaryContent');
 
 test('classNames adds classes', () => {
-  const wrapper = mount(<CardPrimaryContent className='test-class-name'/>);
+  const wrapper = mount(<CardPrimaryContent className='test-class-name' />);
   const primaryContentDiv = wrapper.find('.mdc-card__primary-action');
   assert.isTrue(primaryContentDiv.hasClass('test-class-name'));
   assert.equal(primaryContentDiv.length, 1);
@@ -15,11 +17,13 @@ test('classNames adds classes', () => {
 
 test('it should contain mdc-ripple-upgraded', () => {
   const mockRaf = createMockRaf();
-  const wrapper = mount(<CardPrimaryContent/>);
+  const wrapper = mount(<CardPrimaryContent />);
   mockRaf.flush();
   assert.isTrue(
-    wrapper.update()
+    wrapper
+      .update()
       .find('.mdc-card__primary-action')
-      .hasClass('mdc-ripple-upgraded'));
+      .hasClass('mdc-ripple-upgraded')
+  );
   mockRaf.restore();
 });
