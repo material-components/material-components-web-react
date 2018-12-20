621cd9dd62ff10d7254d074177cb5ff07d5e5e7a
Matt Goo <mattgoo@google.com>
2018-12-20 12:50:20 -1000

    feat(drawer): typescript support  (#494)

---
+++ test/screenshot/drawer/DrawerBelowTopAppBar.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import '../../../packages/drawer/index.scss';
+import './index.scss';
+import TopAppBar, {
+  TopAppBarFixedAdjust,
+// @ts-ignore
+} from '../../../packages/top-app-bar/index';
+import Drawer, {
+  DrawerAppContent,
+  DrawerHeader,
+  DrawerSubtitle,
+  DrawerTitle,
+  DrawerContent,
+} from '../../../packages/drawer/index';
+// @ts-ignore
+import List from './List.js';
+import MaterialIcon from '../../../packages/material-icon/index';
+
+interface DrawerScreenshotTestProps {
+  onClose: () => void;
+  open: boolean;
+  title: string;
+  modal: boolean;
+  dismissible: boolean;
+  renderLoremIpsum: (section: number) => JSX.Element;
+  renderNavigationIcon: () => MaterialIcon | void;
+};
+
+const DrawerScreenshotTest: React.FunctionComponent<DrawerScreenshotTestProps> = ({
+  /* eslint-disable react/prop-types */
+  onClose,
+  open,
+  title,
+  modal,
+  dismissible,
+  renderLoremIpsum,
+  renderNavigationIcon,
+}) => {
+  return (
+    <React.Fragment>
+      <TopAppBar title={title} navigationIcon={renderNavigationIcon()} />
+
+      <TopAppBarFixedAdjust className='drawer-screenshot-test drawer-screenshot-test--is-below'>
+        <Drawer open={open} onClose={onClose} dismissible={dismissible} modal={modal}>
+          <DrawerHeader>
+            <DrawerTitle>Inbox</DrawerTitle>
+            <DrawerSubtitle>ralph@gmail.com</DrawerSubtitle>
+          </DrawerHeader>
+
+          <DrawerContent>
+            <List />
+          </DrawerContent>
+        </Drawer>
+
+        <DrawerAppContent className='drawer-app-content'>
+          {[0, 1, 2, 3, 4, 5].map(renderLoremIpsum)}
+        </DrawerAppContent>
+      </TopAppBarFixedAdjust>
+    </React.Fragment>
+  );
+};
+export default DrawerScreenshotTest;
--- test/screenshot/drawer/permanentBelowTopAppBar.tsx
+++ test/screenshot/drawer/permanentBelowTopAppBar.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import * as React from 'react';
 import DrawerTest from './DrawerTest';
 
-const DismissibleDrawerScreenshotTest = () => {
+const DismissibleDrawerScreenshotTest: React.FunctionComponent = () => {
   return <DrawerTest open isBelow title='Permanent Drawer Below Top App Bar' />;
 };
 
