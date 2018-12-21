87f9c0e4e5ae28f831dae4737db0fb2094c0665a
Matt Goo <mattgoo@google.com>
2018-12-21 13:17:11 -1000

    feat(radio): typescript (#506)

--- test/screenshot/radio/index.tsx
+++ test/screenshot/radio/index.tsx
@@ -1,29 +1,41 @@
-import React from 'react';
+import * as React from 'react';
 import './index.scss';
 import '../../../packages/list/index.scss';
-
 import Radio, {NativeRadioControl} from '../../../packages/radio/index';
 
-class PetsRadio extends React.Component {
-  constructor(props) {
+type PetsRadioProps = {
+  name: string,
+  disabled?: boolean,
+  petValue?: string
+};
+
+type PetsRadioState = {
+  petValue?: string
+};
+
+class PetsRadio extends React.Component<PetsRadioProps, PetsRadioState> {
+  constructor(props: PetsRadioProps) {
     super(props);
     this.state = {
-      petValue: props.petValue, // eslint-disable-line react/prop-types
+      petValue: props.petValue,
     };
   }
 
   render() {
     const {petValue} = this.state;
     const {name, disabled} = this.props; // eslint-disable-line react/prop-types
-    const pets = [{
-      value: 'dogs',
-      label: 'Dogs',
-      id: 'radio-dogs',
-    }, {
-      value: 'cats',
-      label: 'Cats',
-      id: 'radio-cats',
-    }];
+    const pets = [
+      {
+        value: 'dogs',
+        label: 'Dogs',
+        id: 'radio-dogs',
+      },
+      {
+        value: 'cats',
+        label: 'Cats',
+        id: 'radio-cats',
+      },
+    ];
 
     return (
       <div>
@@ -35,7 +47,7 @@ class PetsRadio extends React.Component {
               checked={petValue === pet.value}
               value={pet.value}
               id={`${pet.id}-${name}`}
-              onChange={(e) => this.setState({petValue: e.target.value})}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.setState({petValue: e.target.value})}
             />
           </Radio>
         ))}
@@ -44,14 +56,15 @@ class PetsRadio extends React.Component {
     );
   }
 }
+
 const RadioScreenshotTest = () => {
   return (
     <div>
       <h3>Pet Radio Buttons</h3>
-      <PetsRadio name='pets'/>
+      <PetsRadio name='pets' />
 
       <h3>Preselected Radio Buttons</h3>
-      <PetsRadio name='pets-preselect' petValue={'cats'}/>
+      <PetsRadio name='pets-preselect' petValue='cats' />
 
       <h3>Disabled Radio Buttons</h3>
       <PetsRadio name='pets-disabled' disabled />
--- test/unit/radio/NativeControl.test.tsx
+++ test/unit/radio/NativeControl.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import {assert} from 'chai';
 import {shallow} from 'enzyme';
 import {NativeRadioControl} from '../../../packages/radio/index';
