959c21e53f00b855b5c7b7cfdb599146c0e2ee8d
Matt Goo <mattgoo@google.com>
2018-12-21 13:17:30 -1000

    feat(layout-grid): typescript (#498)

---
+++ test/unit/layout-grid/Row.test.tsx
@@ -0,0 +1,25 @@
+import {assert} from 'chai';
+import {shallow} from 'enzyme';
+import * as React from 'react';
+import {Row} from '../../../packages/layout-grid/index';
+
+suite('LayoutGridRow');
+
+test('classNames adds classes', () => {
+  const wrapper = shallow(<Row className='test-class-name'>Children</Row>, {
+    disableLifecycleMethods: true,
+  });
+  assert.isTrue(wrapper.hasClass('mdc-layout-grid__inner'));
+  assert.isTrue(wrapper.hasClass('test-class-name'));
+});
+
+test('keeps custom props', () => {
+  const wrapper = shallow(
+    <Row disabled={true} type='test-prop'>
+      Children
+    </Row>,
+    {disableLifecycleMethods: true}
+  );
+  assert.isTrue(wrapper.props().disabled);
+  assert.equal(wrapper.props().type, 'test-prop');
+});
