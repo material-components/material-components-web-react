7581d48262ab9b7ebb7451bf742cbe6abffdc0ef
Matt Goo <mattgoo@google.com>
2018-12-21 11:42:50 -1000

    feat(typography): typescript (#521)

--- test/unit/typography/Headline5.test.tsx
+++ test/unit/typography/Headline5.test.tsx
@@ -1,28 +1,34 @@
 import {assert} from 'chai';
 import {shallow} from 'enzyme';
-import React from 'react';
-
+import * as React from 'react';
 import {Headline5} from '../../../packages/typography/index';
 
 suite('TypographyHeadline5');
 
 test('classNames adds classes', () => {
-  const wrapper = shallow(<Headline5 className='test-class-name'>Text</Headline5>, {disableLifecycleMethods: true});
+  const wrapper = shallow(
+    <Headline5 className='test-class-name'>Text</Headline5>,
+    {disableLifecycleMethods: true}
+  );
   assert.isTrue(wrapper.hasClass('mdc-typography'));
   assert.isTrue(wrapper.hasClass('mdc-typography--headline5'));
   assert.isTrue(wrapper.hasClass('test-class-name'));
 });
 
 test('renders a different tag', () => {
-  const wrapper = shallow(<Headline5 tag="p">Text</Headline5>, {disableLifecycleMethods: true});
+  const wrapper = shallow(<Headline5 tag='p'>Text</Headline5>, {
+    disableLifecycleMethods: true,
+  });
   assert.equal(wrapper.type(), 'p');
 });
 
 test('keeps custom props', () => {
   const wrapper = shallow(
-    <Headline5 propOne={true} propTwo='test-prop'>Children</Headline5>,
-    {disableLifecycleMethods: true},
+    <Headline5 disabled={true} label='test-prop'>
+      Children
+    </Headline5>,
+    {disableLifecycleMethods: true}
   );
-  assert.isTrue(wrapper.props().propOne);
-  assert.equal(wrapper.props().propTwo, 'test-prop');
+  assert.isTrue(wrapper.props().disabled);
+  assert.equal(wrapper.props().label, 'test-prop');
 });
