a25039e1e0d2b3255d4edb2ee90114a7ecbeac52
Matt Goo <mattgoo@google.com>
2018-12-21 13:17:30 -1000

    feat(layout-grid): typescript (#498)

--- test/unit/layout-grid/Grid.test.tsx
+++ test/unit/layout-grid/Grid.test.tsx
@@ -1,31 +1,41 @@
 import {assert} from 'chai';
 import {shallow} from 'enzyme';
-import React from 'react';
-
+import * as React from 'react';
 import {Grid} from '../../../packages/layout-grid/index';
 
 suite('LayoutGridGrid');
 
 test('classNames adds classes', () => {
-  const wrapper = shallow(<Grid className='test-class-name'>Children</Grid>, {disableLifecycleMethods: true});
+  const wrapper = shallow(<Grid className='test-class-name'>Children</Grid>, {
+    disableLifecycleMethods: true,
+  });
   assert.isTrue(wrapper.hasClass('mdc-layout-grid'));
   assert.isTrue(wrapper.hasClass('test-class-name'));
 });
 
 test('align prop adds correct className', () => {
-  const wrapper = shallow(<Grid align='right'>Children</Grid>, {disableLifecycleMethods: true});
+  const wrapper = shallow(<Grid align='right'>Children</Grid>, {
+    disableLifecycleMethods: true,
+  });
   assert.isTrue(wrapper.hasClass('mdc-layout-grid'));
   assert.isTrue(wrapper.hasClass('mdc-layout-grid--align-right'));
 });
 
 test('fixedColumnWidth prop adds correct className', () => {
-  const wrapper = shallow(<Grid fixedColumnWidth>Children</Grid>, {disableLifecycleMethods: true});
+  const wrapper = shallow(<Grid fixedColumnWidth>Children</Grid>, {
+    disableLifecycleMethods: true,
+  });
   assert.isTrue(wrapper.hasClass('mdc-layout-grid'));
   assert.isTrue(wrapper.hasClass('mdc-layout-grid--fixed-column-width'));
 });
 
 test('keeps custom props', () => {
-  const wrapper = shallow(<Grid propOne={true} propTwo='test-prop'>Children</Grid>, {disableLifecycleMethods: true});
-  assert.isTrue(wrapper.props().propOne);
-  assert.equal(wrapper.props().propTwo, 'test-prop');
+  const wrapper = shallow(
+    <Grid disabled={true} label='test-prop'>
+      Children
+    </Grid>,
+    {disableLifecycleMethods: true}
+  );
+  assert.isTrue(wrapper.props().disabled);
+  assert.equal(wrapper.props().label, 'test-prop');
 });
