a0709d7f9274d68adf0a50ef6078f52772e411b2
Matt Goo <mattgoo@google.com>
2018-12-27 10:01:25 -1000

    feat(tab-bar): typescript (#535)

--- test/screenshot/tab-bar/index.tsx
+++ test/screenshot/tab-bar/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import Tab from '../../../packages/tab/index';
+import * as React from 'react';
+import Tab, {TabProps} from '../../../packages/tab/index'; // eslint-disable-line no-unused-vars
 import TabBar from '../../../packages/tab-bar/index';
-
 import './index.scss';
 
-class TabBarTest extends React.Component {
+interface TabBarTestProps {
+  activeIndex?: number;
+  className?: string;
+  indicatorContent?: React.ReactNode;
+  isFadingIndicator?: boolean;
+  numTabs: number;
+  isRtl?: boolean;
+  hasUpdateActiveIndexButton?: boolean;
+};
+
+interface TabBarTestState {
+  activeIndex: number
+};
+
+class TabBarTest extends React.Component<TabBarTestProps, TabBarTestState> {
   state = {
-    activeIndex: this.props.activeIndex || 0, // eslint-disable-line react/prop-types
+    activeIndex: this.props.activeIndex || 0,
   };
 
-  handleActiveIndexUpdate = (activeIndex) => this.setState({activeIndex});
+  handleActiveIndexUpdate = (activeIndex: number) => this.setState({activeIndex});
 
   render() {
     const {
@@ -20,10 +33,9 @@ class TabBarTest extends React.Component {
       numTabs,
       isRtl,
       hasUpdateActiveIndexButton,
-      /* eslint-enable react/prop-types */
     } = this.props;
 
-    const renderTab = (num, index) => {
+    const renderTab: (num: number, index: number) => React.ReactElement<TabProps> = (num, index) => {
       return (
         <Tab
           key={index}
@@ -35,6 +47,10 @@ class TabBarTest extends React.Component {
       );
     };
 
+    let tabList = new Array(numTabs);
+    tabList = tabList.fill(null).map((_, i) => i);
+
+
     return (
       <React.Fragment>
         <TabBar
@@ -43,15 +59,17 @@ class TabBarTest extends React.Component {
           activeIndex={this.state.activeIndex}
           handleActiveIndexUpdate={this.handleActiveIndexUpdate}
         >
-          {[...Array(numTabs).keys()].map(renderTab)}
+          {tabList.map(renderTab)}
         </TabBar>
-        {hasUpdateActiveIndexButton ?
-          <button onClick={() => this.setState({activeIndex: 1})}>Activate tab at index 1</button> : null}
+        {hasUpdateActiveIndexButton ? (
+          <button onClick={() => this.setState({activeIndex: 1})}>
+            Activate tab at index 1
+          </button>
+        ) : null}
       </React.Fragment>
     );
   }
 }
-
 const TabBarScreenshotTest = () => {
   return (
     <div>
@@ -59,40 +77,36 @@ const TabBarScreenshotTest = () => {
       <div className='tab-bar-container'>
         <TabBarTest numTabs={3} hasUpdateActiveIndexButton />
       </div>
-
       Sliding Underline
       <div className='tab-bar-container'>
         <TabBarTest numTabs={3} />
       </div>
-
       Fading Underline
       <div className='tab-bar-container'>
-        <TabBarTest numTabs={3}
-          activeIndex={1}
-          isFadingIndicator />
+        <TabBarTest numTabs={3} activeIndex={1} isFadingIndicator />
       </div>
-
       Sliding Icon
       <div className='tab-bar-container'>
-        <TabBarTest numTabs={3}
+        <TabBarTest
+          numTabs={3}
           className='icon-indicator-tab-bar'
           activeIndex={2}
-          indicatorContent={<i className='material-icons'>star_border</i>} />
+          indicatorContent={<i className='material-icons'>star_border</i>}
+        />
       </div>
-
       Fading Icon
       <div className='tab-bar-container'>
-        <TabBarTest numTabs={3}
+        <TabBarTest
+          numTabs={3}
           className='icon-indicator-tab-bar'
           isFadingIndicator
-          indicatorContent={<i className='material-icons'>favorite</i>} />
+          indicatorContent={<i className='material-icons'>favorite</i>}
+        />
       </div>
-
       Scrolling Tabs
       <div className='tab-bar-container'>
         <TabBarTest numTabs={20} />
       </div>
-
       Scrolling RTL Tabs
       <div className='tab-bar-container'>
         <TabBarTest numTabs={20} isRtl />
@@ -100,5 +114,4 @@ const TabBarScreenshotTest = () => {
     </div>
   );
 };
-
 export default TabBarScreenshotTest;
