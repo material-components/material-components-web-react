19617ceb97e5cb1101f6fadbd98131d2df7f6262
Andrii Kostenko <andrey@kostenko.name>
2019-01-16 14:08:35 +0300

    Dialog component

---
+++ packages/dialog/README.md
@@ -0,0 +1,64 @@
+# React Button
+
+A React version of an [MDC Button](https://github.com/material-components/material-components-web/tree/master/packages/mdc-button).
+
+## Installation
+
+```
+npm install @material/react-button
+```
+
+## Usage
+
+### Styles
+
+with Sass:
+```js
+import '@material/react-button/index.scss';
+```
+
+with CSS:
+```js
+import '@material/react-button/dist/button.css';
+```
+
+### Javascript Instantiation
+```js
+import React from 'react';
+import Button from '@material/react-button';
+
+class MyApp extends React.Component {
+  render() {
+    return (
+      <Button>
+        Click Me!
+      </Button>
+    );
+  }
+}
+```
+
+## Props
+
+Prop Name | Type | Description
+--- | --- | ---
+className | String | Classes to be applied to the root element.
+raised | Boolean | Enables raised variant.
+unelevated | Boolean | Enables unelevated variant.
+outlined | Boolean | Enables outlined variant.
+dense | Boolean | Enables dense variant.
+icon | Element | Icon to render within root element.
+children | String | Text to be displayed within root element.
+disabled | Boolean | Disables button if true.
+href | String | Sets a hyperlink & uses anchor tag instead of a button.
+
+## Sass Mixins
+
+Sass mixins may be available to customize various aspects of the Components. Please refer to the
+MDC Web repository for more information on what mixins are available, and how to use them.
+
+[Advanced Sass Mixins](https://github.com/material-components/material-components-web/blob/master/packages/mdc-button/README.md#sass-mixins)
+
+## Usage with Icons
+
+Please see our [Best Practices doc](../../docs/best-practices.md#importing-font-icons) when importing or using icon fonts.
---
+++ packages/dialog/index.tsx
@@ -0,0 +1,181 @@
+// The MIT License
+//
+// Copyright (c) 2018 Google, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import * as React from 'react';
+import createFocusTrap from 'focus-trap';
+import * as classnames from 'classnames';
+import { MDCDialogFoundation, MDCDialogAdapter } from '@material/dialog';
+import { IMDCDialogAdapter } from './types';
+
+type Props = {
+  className?: string,
+  children: React.ReactChild,
+  title?: string | null,
+  actions?: React.ReactChild,
+  onRequestClose?: () => void,
+};
+
+type State = {
+  bodyClassList: Set<string>,
+  classList: Set<string>,
+  focusTrap: any,
+  ref: HTMLDivElement | null,
+  resizeListener?: (e: Event) => void,
+  keydownListener?: (e: Event) => void,
+}
+
+
+export class Dialog extends React.Component<Props, State> {
+  dialog: any
+  focusTrapFactory_: any
+  constructor(props: Props) {
+      super(props);
+      this.focusTrapFactory_ = createFocusTrap;
+      this.state = {
+          classList: new Set(),
+          bodyClassList: new Set(),
+          ref: null,
+          focusTrap: null,
+      };
+  }
+  dialogRef = (ref: HTMLDivElement) => {
+      this.setState({
+          ref,
+          focusTrap: this.focusTrapFactory_(ref, {
+              initialFocus: null,
+              escapeDeactivates: false,
+              clickOutsideDeactivates: true,
+          }),
+      });
+  }
+  onDialogClosed = () => {
+      this.props.onRequestClose && this.props.onRequestClose();
+  }
+  componentDidMount() {
+      this.dialog = new MDCDialogFoundation(this.adapter as unknown as MDCDialogAdapter);
+      this.dialog.init();
+      this.dialog.open();
+      const keydownListener: EventListener = (e: Event) => this.dialog.handleDocumentKeydown(e);
+      const resizeListener: EventListener = (e: Event) => this.dialog.layout(e);
+      this.setState({
+          keydownListener,
+          resizeListener,
+      });
+  }
+  componentWillUnmount() {
+      const { keydownListener, resizeListener } = this.state;
+      if (keydownListener)
+          document.removeEventListener('keydown', keydownListener);
+      if (resizeListener)
+          window.removeEventListener('resize', resizeListener);
+      this.dialog.destroy();
+  }
+  handleDialogClick = (e: React.SyntheticEvent) => {
+      this.dialog.handleInteraction(e.nativeEvent);
+  }
+  get adapter():IMDCDialogAdapter {
+      return {
+          addClass: (className: string) => {
+              const classList = new Set(this.state.classList);
+              classList.add(className);
+              this.setState({ classList });
+          },
+          removeClass: (className: string) => {
+              const classList = new Set(this.state.classList);
+              classList.delete(className);
+              this.setState({ classList });
+          },
+          trapFocus: () => this.state.focusTrap.activate(),
+          releaseFocus: () => this.state.focusTrap.deactivate(),
+          eventTargetMatches: (target: HTMLElement, selector: string) => {
+              return target.matches(selector);
+          },
+          isContentScrollable() {
+            // TODO
+            return false;
+          },
+          areButtonsStacked() {
+            // TODO
+            return false;
+          },
+          reverseButtons() {
+            // TODO
+          },
+          clickDefaultButton() {
+            // TODO
+          },
+          notifyOpening() {
+            // TODO
+          },
+          notifyOpened() {
+            // TODO
+          },
+          notifyClosing() {
+            // TODO
+          },
+          getActionFromEvent() {
+            // TODO
+            return 'unknown';
+          },
+          notifyClosed: () => {
+              this.onDialogClosed();
+          },
+          addBodyClass: (className: string) => {
+              document.body && document.body.classList.add(className);
+
+          },
+          removeBodyClass: (className: string) => {
+              document.body && document.body.classList.remove(className);
+          },
+          hasClass: (className: string) => this.classes.split(' ').includes(className),
+      };
+  }
+  get classes() {
+      const { classList } = this.state;
+      return classnames('mdc-dialog', Array.from(classList));
+  }
+  get bodyClasses() {
+      const { bodyClassList } = this.state;
+      return classnames('mdc-dialog__content', Array.from(bodyClassList));
+  }
+  render() {
+      return(<div className={this.classes} onClick={this.handleDialogClick}
+          role="alertdialog"
+          aria-modal="true"
+          ref={this.dialogRef}>
+          <div className="mdc-dialog__container">
+              <div className="mdc-dialog__surface">
+                  {this.props.title ? <h2 className="mdc-dialog__title">{this.props.title}</h2> : null }
+                  <div className={this.bodyClasses}>
+                      {this.props.children}
+                  </div>
+                  <footer className="mdc-dialog__actions">
+                      {this.props.actions}
+                  </footer>
+              </div>
+          </div>
+          <div className="mdc-dialog__scrim" onClick={this.handleDialogClick}></div>
+      </div>);
+  }
+}
+
+export default Dialog;
