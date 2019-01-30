5fc8cb8fe7c466081571fa09af225203c1ba777d
aluminick <archuanickie@gmail.com>
2019-01-30 21:27:55 +0800

    removed commented code

--- packages/ripple/index.tsx
+++ packages/ripple/index.tsx
@@ -260,9 +260,7 @@ export function withRipple <
   activateRipple = (e: ActivateEventTypes<Surface>) => {
     // https://reactjs.org/docs/events.html#event-pooling
     e.persist();
-    /* requestAnimationFrame(() => { */
     this.foundation.activate(e)
-    /* }); */
   };
 
   deactivateRipple = (e: ActivateEventTypes<Surface>) => {
