86294768cf7cc2baaa2674c60ec2741259b31bc8
mgr34 <matthew.g.roth@yale.edu>
2019-01-31 14:36:04 -0500

    fix(chips): ChipSet no longer overwrites handle[Interaction|Select|Remove] of Chip

--- packages/chips/ChipSet.tsx
+++ packages/chips/ChipSet.tsx
@@ -163,14 +163,24 @@ export default class ChipSet extends React.Component<ChipSetProps, ChipSetState>
     const {filter} = this.props;
     const {selectedChipIds} = this.state;
     const selected = selectedChipIds.indexOf(chip.props.id) > -1;
+    const {handleInteraction, handleSelect, handleRemove, ...chipProps} = chip.props;
     const props = Object.assign(
       {},
-      ...chip.props,
+      ...chipProps,
       {
         selected,
-        handleSelect: this.handleSelect,
-        handleInteraction: this.handleInteraction,
-        handleRemove: this.handleRemove,
+        handleSelect: (id: string, selected: boolean): void => {
+          handleSelect!(id, selected);
+          this.handleSelect(id, selected);
+        },
+        handleInteraction: (id: string): void => {
+          handleInteraction!(id);
+          this.handleInteraction(id);
+        },
+        handleRemove: (id: string): void => {
+          handleRemove!(id);
+          this.handleRemove(id);
+        },
         chipCheckmark: filter ? (
           <ChipCheckmark ref={this.setCheckmarkWidth} />
         ) : null,
