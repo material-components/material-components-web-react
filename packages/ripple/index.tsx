57b55b1a8cc67552b9b8495f9283e1972abd4020
aluminick <archuanickie@gmail.com>
2019-02-07 02:40:46 +0800

    fixed missing semicolon

--- packages/ripple/index.tsx
+++ packages/ripple/index.tsx
@@ -264,7 +264,7 @@ export function withRipple <
   activateRipple = (e: ActivateEventTypes<Surface>) => {
     // https://reactjs.org/docs/events.html#event-pooling
     e.persist();
-    this.foundation.activate(e)
+    this.foundation.activate(e);
   };
 
   deactivateRipple = (e: ActivateEventTypes<Surface>) => {
