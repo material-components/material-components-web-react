8fa383e2ef6cddec8dc5cbfc7486b7f3ff80ef38
Matt Goo <mattgoo@google.com>
2019-04-02 16:24:45 -0700

    fix(list): maintain classes with state.listItemClassNames (#776)
    BREAKING CHANGE: Removes props.tabbableOnListItemFocus from all the auxiliary components, as it seemed confusing to have this and tabIndex dictate what tabIndex would ultimately be.

--- test/unit/list/ListItemGraphic.test.tsx
+++ test/unit/list/ListItemGraphic.test.tsx
@@ -16,17 +16,3 @@ test('has mdc-list-item__graphic class', () => {
   const wrapper = shallow(<ListItemGraphic graphic={<i />} />);
   assert.isTrue(wrapper.hasClass('mdc-list-item__graphic'));
 });
-
-test('has tabIndex of props.tabIndex if specified and tabbableOnListItemFocus is true', () => {
-  const wrapper = shallow(
-    <ListItemGraphic graphic={<i />} tabIndex={3} tabbableOnListItemFocus />
-  );
-  assert.equal(wrapper.find('.mdc-list-item__graphic').props().tabIndex, 3);
-});
-
-test('has tabIndex of -1 if tabbableOnListItemFocus is false', () => {
-  const wrapper = shallow(
-    <ListItemGraphic graphic={<i />} childrenTabIndex={3} />
-  );
-  assert.equal(wrapper.find('.mdc-list-item__graphic').props().tabIndex, -1);
-});
