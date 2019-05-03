480dbd6c50b1c3ff965e06977314cf93bbd0ecf9
Troy Tae <tjy970721@gmail.com>
2019-05-03 21:26:35 +0900

    feat(layout-grid): change literal to constant

--- packages/layout-grid/Row.tsx
+++ packages/layout-grid/Row.tsx
@@ -23,6 +23,8 @@
 import React from 'react';
 import classnames from 'classnames';
 
+import {CSS_CLASSES} from './constant';
+
 export interface RowProps<T> extends React.HTMLProps<T> {
   className?: string,
   tag?: string
@@ -36,7 +38,7 @@ const Row: <T extends {} = HTMLDivElement>(props: RowProps<T>) => React.ReactEle
   ...otherProps
   /* eslint-enable react/prop-types */
 }) => {
-  const classes = classnames('mdc-layout-grid__inner', className);
+  const classes = classnames(CSS_CLASSES.INNER, className);
 
   return (
     // https://github.com/Microsoft/TypeScript/issues/28892
