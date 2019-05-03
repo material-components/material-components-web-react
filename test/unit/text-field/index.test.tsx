970be9c71fff864b4dfe205f7cad6c165dd3ee1a
Troy Tae <tjy970721@gmail.com>
2019-05-04 01:36:04 +0900

    feat(text-field): create error test case

--- test/unit/text-field/index.test.tsx
+++ test/unit/text-field/index.test.tsx
@@ -696,3 +696,21 @@ test('Useless test for code coverage', () => {
   wrapper.instance().adapter.registerInputInteractionHandler(temp, temp);
   wrapper.instance().adapter.deregisterInputInteractionHandler(temp, temp);
 });
+
+test('Input component sync test in TextField', () => {
+  class TestComponent extends React.Component {
+    state = {
+      disabled: false,
+    };
+    render() {
+      return <TextField>
+        <Input disabled={this.state.disabled} />
+      </TextField>;
+    }
+  }
+  const wrapper = mount<TestComponent>(<TestComponent/>);
+  wrapper.instance().setState({disabled: true});
+  // If input component is null in TextField,
+  // setDisabled called #inputAdapter.getNativeInput
+  // and throw error because there is no inputComponent
+});
