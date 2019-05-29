6343cd104e644c6422b0a936449cd39af221a9f7
TroyTae <tjy970721@gmail.com>
2019-05-29 23:46:00 +0900

    test(list): test for template and textarea

--- test/screenshot/text-field/characterCounter.tsx
+++ test/screenshot/text-field/characterCounter.tsx
@@ -14,6 +14,22 @@ class CharacterCounterTextField extends React.Component<{}, {value: string}> {
             maxLength={150}
           />
         </TextField>
+        <TextField label='HaHa' characterCounter={<CharacterCounter template='현재(${count}) / 최대(${maxLength})' />}>
+          <Input
+            value='Happy Coding'
+            maxLength={150}
+          />
+        </TextField>
+        <TextField
+          textarea
+          label='HaHa'
+          characterCounter={<CharacterCounter template='현재(${count}) / 최대(${maxLength})' />}
+        >
+          <Input
+            value='Happy Coding'
+            maxLength={150}
+          />
+        </TextField>
       </div>
     );
   }
