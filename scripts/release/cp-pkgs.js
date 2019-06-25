08580e6aad14db97552b46af5324688604a6e9e8
Ari Chivukula <ari@chivuku.la>
2019-06-25 07:25:14 -0700

    Update cp-pkgs.js

--- scripts/release/cp-pkgs.js
+++ scripts/release/cp-pkgs.js
@@ -95,7 +95,7 @@ function addTsIgnore(filePath) {
 // and copies file into destination directory
 function cpTypes(typeAsset) {
   const {dir, base} = path.parse(typeAsset);
-  let destDir = dir.split('build/types/')[1];
+  let destDir = dir.split('types/')[1];
   destDir = destDir.split('/');
   destDir.splice(2, 0, 'dist');
   destDir = `${destDir.join('/')}/${base}`;
@@ -108,7 +108,7 @@ async function copyPackages() {
   try {
     await Promise.all(globSync('build/*.{css,js,map}').map(cpAsset));
     console.log('-- Copied CSS, JS, and Map Files to Destination Directory');
-    await Promise.all(globSync('build/types/packages/**/*.d.ts').map(cpTypes));
+    await Promise.all(globSync('types/packages/**/*.d.ts').map(cpTypes));
     console.log('-- Copied Typescript Type Files to Destination Directory');
   } catch (err) {
     console.error(`Error encountered copying assets: ${err}`);
