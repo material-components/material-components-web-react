a26c647e78348b32739517e5491d2d99a98eeff9
Ari Chivukula <ari@chivuku.la>
2019-06-28 11:29:13 -0700

    Update Chip.tsx

--- packages/chips/Chip.tsx
+++ packages/chips/Chip.tsx
@@ -24,7 +24,8 @@ import classnames from 'classnames';
 import {
   withRipple,
   InjectedProps,
-  RippledComponentProps, // @ts-ignore TODO(issues/955) Remove once possible
+  // @ts-ignore TODO(issues/955) Remove once possible
+  RippledComponentProps, // eslint-disable-line @typescript-eslint/no-unused-vars
 } from '@material/react-ripple';
 import {MDCChipFoundation} from '@material/chips/chip/foundation';
 import {MDCChipAdapter} from '@material/chips/chip/adapter';
